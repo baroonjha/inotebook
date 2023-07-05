f0037ec481fe9e6f16d80d1ea9cc2d8251c86570
baroonjha <85762785+baroonjha@users.noreply.github.com>
2023-07-05 14:06:33 +0530

    created auth.js and added express validator for validation

--- Backend/db.js
+++ Backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const mongoUri = "mongodb://localhost:27017"
+const mongoUri = "mongodb://localhost:27017/inotebook"
 
 const connectToMongo =async ()=>{
     mongoose.connect(mongoUri,await console.log('Connected to mongoose'))
